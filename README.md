6eac0bebc5b62e6783c7d6914203a8fbe5dc16ca
muhammad-fiaz <contact@muhammadfiaz.com>
2023-06-29 12:00:00 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # rotating-donut-shaped-in-c-programming
 The spinning “donuts” has brought some attention recently. No wonder, a flying donut on your terminal? You can find the code here in C programming. The “pixels” are ASCII characters .,-~:;=!*#$@ that accounts for the illumination value of the surface.
 
-# Output
+## Output
 
 ![donut](https://user-images.githubusercontent.com/75434191/192341324-babc2875-ed83-4279-b903-fbf737b5da70.gif)
+
+## How to Run
+
+To run this project, you will need to have CMake installed on your system. Follow these steps:
+
+1. **Clone the Repository**:
+    ```bash
+    git clone https://github.com/muhammad-fiaz/rotating-donut-shaped-in-c-programming.git
+    cd rotating-donut-shaped-in-c-programming
+    ```
+
+2. **Create a Build Directory**:
+    ```bash
+    mkdir build
+    cd build
+    ```
+
+3. **Generate the Build Files with CMake**:
+    ```bash
+    cmake ..
+    ```
+
+4. **Build the Project**:
+    ```bash
+    make
+    ```
+
+5. **Run the Executable**:
+    ```bash
+    ./rotating_donut
+    ```
+
+This will compile and run the rotating donut program on your terminal.
+
+
+## Acknowledgements
+
+- Thanks to Andy Sloane for providing the inspiration and mathematical foundation for this project. You can find his detailed explanation and original code on his [website](https://www.a1k0n.net/2011/07/20/donut-math.html).
+- Thanks to the various developers and enthusiasts who have shared and modified this code over the years, making it accessible and enjoyable for everyone.
+
+
+
+
+
--- donut.c
+++ donut.c
@@ -1,21 +1,25 @@
-#include<stdio.h>   //standard library
-#include<string.h> //used for string 
-#include <math.h> //used for mathematical operations like sin,cos
+#include <stdio.h>   // Standard library
+#include <string.h>  // Used for string operations
+#include <math.h>    // Used for mathematical operations like sin, cos
+#include <unistd.h>  // Used for usleep function
 
-int main() {  //main
-    float A = 0, B = 0;
+int main() {  // Main function
+    float A = 0, B = 0;  // Rotation angles
     float i, j;
     int k;
-    float z[1760];
-    char b[1760];
-     printf("\x1b[2J");  //\x1b[2J is the code for clearing the screen and set cursor to home
-    //  system("cls");  //to clear console screen
+    float z[1760];  // Z-buffer
+    char b[1760];   // Buffer for storing the ASCII characters
 
-    for(;;) { //loop
-        memset(b,32,1760); 
-        memset(z,0,7040);
-        for(j=0; j < 6.28; j += 0.07) {  //loop
-            for(i=0; i < 6.28; i += 0.02) {  //loop
+    // Clear the screen and set cursor to home
+    printf("\x1b[2J");
+
+    for(;;) { // Infinite loop to keep the donut spinning
+        memset(b, 32, 1760);  // Fill buffer with spaces
+        memset(z, 0, 7040);   // Reset Z-buffer
+
+        for(j = 0; j < 6.28; j += 0.07) {  // Loop over the donut's latitude
+            for(i = 0; i < 6.28; i += 0.02) {  // Loop over the donut's longitude
+                // Calculate the coordinates of the surface point
                 float c = sin(i);
                 float d = cos(j);
                 float e = sin(A);
@@ -28,22 +32,29 @@ int main() {  //main
                 float n = sin(B);
                 float t = c * h * g - f * e;
                 int x = 40 + 30 * D * (l * h * m - t * n);
-                int y= 12 + 15 * D * (l * h * n + t * m);
+                int y = 12 + 15 * D * (l * h * n + t * m);
                 int o = x + 80 * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {  //condition
+
+                // Check if the point is within bounds and update the buffers
+                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
             }
         }
-        printf("\x1b[H");   //\x1b[H is the code for returning the cursor to the home position.
-        for(k = 0; k < 1761; k++) {  //loop
+
+        // Return the cursor to the home position
+        printf("\x1b[H");
+
+        // Print the buffer
+        for(k = 0; k < 1761; k++) {
             putchar(k % 80 ? b[k] : 10);
-            A += 0.00004;
-            B += 0.00002;
+            A += 0.00004;  // Increment the rotation angle A
+            B += 0.00002;  // Increment the rotation angle B
         }
-        usleep(30000);
+        usleep(30000);  // Sleep for a short period to control the rotation speed
     }
-    return 0; //return
+
+    return 0; // Return
 }
